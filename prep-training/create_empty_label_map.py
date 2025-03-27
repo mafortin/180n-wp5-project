d334a22b8e346391410baf36c484fccc03094a07
mafortin <marc.a.fortin@ntnu.no>
2025-03-27 12:42:42 +0000

    Modified the create_empty_label_map.py script such that it nows creates np.int16 data precision instead of np.float32 to reduce storage size.

--- prep-training/create_empty_label_map.py
+++ prep-training/create_empty_label_map.py
@@ -9,22 +9,26 @@ import numpy as np
 def create_empty_label_map(input_dir, reference_filename, output_filename):
     # Loop through all subdirectories in the input directory
     for root, dirs, files in os.walk(input_dir):
+
+
         for subdir in dirs:
+            
+            # Extract the sub_id from the subfolder name
+            sub_id = subdir
             subdir_path = os.path.join(root, subdir)
             
-            # Check if the output file already exists
-            output_file_path = os.path.join(subdir_path, output_filename)
+            # Update the output filename to include the sub_id
+            output_file_path = os.path.join(subdir_path, f"{sub_id}{output_filename}")
+
             if not os.path.exists(output_file_path):
                 # Check if the reference file exists
-                reference_file_path = os.path.join(subdir_path, reference_filename)
+                reference_file_path = os.path.join(subdir_path,  f"{sub_id}{reference_filename}")
                 if os.path.exists(reference_file_path):
-                    # Load the reference file to get its shape and affine
                     reference_image = nib.load(reference_file_path)
                     reference_data = reference_image.get_fdata()
                     affine = reference_image.affine
-                    
                     # Create a 3D matrix of zeros with the same shape as the reference
-                    empty_data = np.zeros_like(reference_data)
+                    empty_data = np.zeros_like(reference_data, dtype=np.int16)  # Use integer type
                     
                     # Create a new NIfTI image with the empty data
                     empty_image = nib.Nifti1Image(empty_data, affine)
