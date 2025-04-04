1629239c7e61296ac8b5b09ac1b89462b437da24
mafortin <marc.a.fortin@ntnu.no>
2025-04-04 16:32:23 +0000

    Now includes the option to run multiple configurations

--- training/script_nnunet_fold_trainings.py
+++ training/script_nnunet_fold_trainings.py
@@ -2,9 +2,9 @@ import subprocess
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser(description="Automate nnUNetv2 training for multiple datasets and plans.")
+    parser = argparse.ArgumentParser(description="Automate nnUNetv2 training for multiple datasets, configurations, and plans.")
     parser.add_argument('--datasets', nargs='+', required=True, help='List of dataset identifiers')
-    parser.add_argument('--configuration', required=True, default='3d_fullres', help='Configuration (e.g., 3d_fullres)')
+    parser.add_argument('--configurations', nargs='+', required=True, help='List of configurations (e.g., 3d_fullres, 2d)')
     parser.add_argument('--trainer', required=True, nargs='+', help='Trainer (e.g., nnUNetTrainerNoDA)')
     parser.add_argument('--plans', nargs='+', required=True, help='List of plans')
     parser.add_argument('--debug', action='store_true', help='Enable debug mode to print commands without executing them')
@@ -15,10 +15,11 @@ def main():
     start_fold = args.start_fra_fold if args.start_fra_fold is not None else 0
 
     for dataset in args.datasets:
-        for plan in args.plans:
-            for trainer in args.trainer:
-                for fold in range(start_fold, 5):
-                    run_training(dataset, args.configuration, trainer, fold, plan, args.debug)
+        for configuration in args.configurations:
+            for plan in args.plans:
+                for trainer in args.trainer:
+                    for fold in range(start_fold, 5):
+                        run_training(dataset, configuration, trainer, fold, plan, args.debug)
 
     print("All trainings have been completed.")
 
@@ -34,14 +35,14 @@ def run_training(dataset, configuration, trainer, fold, plan, debug=False):
     else:
         try:
             print('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
-            print(f"Starting training for dataset {dataset}, fold {fold}, plan {plan}")
+            print(f"Starting training for dataset {dataset}, configuration {configuration}, fold {fold}, plan {plan}")
             result = subprocess.run(command, check=True, capture_output=True, text=True)
-            print(f"Finished training for dataset {dataset}, fold {fold}, plan {plan}")
+            print(f"Finished training for dataset {dataset}, configuration {configuration}, fold {fold}, plan {plan}")
             print('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
             print(result.stdout)
         except subprocess.CalledProcessError as e:
             print('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
-            print(f"~~~~~~!!!!!!!!!!!Training for dataset {dataset}, fold {fold}, plan {plan} FAILED with error: {e.stderr}!!!!!!!!!!!!~~~~~~~")
+            print(f"~~~~~~!!!!!!!!!!!Training for dataset {dataset}, configuration {configuration}, fold {fold}, plan {plan} FAILED with error: {e.stderr}!!!!!!!!!!!!~~~~~~~")
             print('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
 
 if __name__ == "__main__":
