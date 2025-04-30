5da245638068a2b34d89c27a4ae51b2e55bf6b07
mafortin <marc.a.fortin@ntnu.no>
2025-04-30 13:21:20 +0000

    Small modifications

--- post-training/get_evaluation_metrics.py
+++ post-training/get_evaluation_metrics.py
@@ -2,10 +2,13 @@ from picai_eval import Metrics
 import matplotlib.pyplot as plt
 from sklearn.metrics import PrecisionRecallDisplay, RocCurveDisplay
 
-
+# Provide the full path to the JSON file containing the PICAI metrics
 path2picai_metrics = "/home/marcantf/Data/nnunet/results/Dataset016_180n_lesion/my_nnUNetTrainer__UNet_ResEncL__3d_fullres/inference/picai_metrics.json"
+show_plots = True #False
 
+# Fetch the metrics
 metrics = Metrics(path2picai_metrics)
+
 
 # aggregate metrics
 AP = metrics.AP
@@ -24,22 +27,33 @@ fpr = metrics.case_FPR
 sensitivity = metrics.lesion_TPR
 fp_per_case = metrics.lesion_FPR
 
+### Print metrics ###
+print('AUROC:', auroc)
+print('AP:', AP)
+print('PICAI score:', picai_score)
+print('Precision:', precision)
+print('Recall:', recall)
+
 ### Plotting section ###
 
 # Pecision-Recall (PR) curve
 disp = PrecisionRecallDisplay(precision=precision, recall=recall, average_precision=AP)
 disp.plot()
-plt.show()
+if show_plots:
+    plt.show()
 
 # Receiver Operating Characteristic (ROC) curve
 disp = RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=auroc)
 disp.plot()
-plt.show()
+if show_plots:
+    plt.show()
 
 # Free-Response Receiver Operating Characteristic (FROC) curve
 f, ax = plt.subplots()
 disp = RocCurveDisplay(fpr=fp_per_case, tpr=sensitivity)
 disp.plot(ax=ax)
-ax.set_xlim(0.001, 5.0); ax.set_xscale('log')
-ax.set_xlabel("False positives per case"); ax.set_ylabel("Sensitivity")
-plt.show()
+
+#if show_plots:
+#    ax.set_xlim(0.001, 5.0); ax.set_xscale('log')
+#    ax.set_xlabel("False positives per case"); ax.set_ylabel("Sensitivity")
+#    plt.show()
